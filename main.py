748a6f34fec4612b3a2f04b6d3044b58b6eb6084
Jimenez <CFJimenez@cps.k12.il.us>
2024-04-29 08:40:44 -0500

    learning basics

---
+++ main.py
@@ -0,0 +1,34 @@
+# print("helow world")
+# # print is a function that prints out a message to the console
+# # strings are surrounded by quotes
+# # single or double qoutes '' or ""
+# # whenever a word is surrounded by qoutes it is balled a string
+# # be consistent with the qoutes you use
+# print("Cristal")
+# print("order of execution")
+# print("in python")
+# print("*"*20)
+# # varivels are used to store data
+# # variables are created when you assign a value to it
+# # variables are case sensitive
+# price = 10 #integar variables
+# name = "John" #sting variables
+# rating = 4.9 #float variables
+# is_published = True #boolean variables
+# print("name")
+# print("price")
+# print("rating")
+# # string interpolation where you can use variables in a sentence
+# print( name +" is a basketball player ")
+# # concatenation to join variables in a sentence using 
+# # the plus (+) sign 
+# print(name + " has a rating of " + str(rating))
+# # use the str()function to convert to a number to a string
+# #  this is called type conversion 
+# print("the price of the book is " + str(price))
+# # getting input from the user
+
+name = input("What is your name? ")
+age =("What is your age? ")
+occupation = ("What is your occuation? ")
+print("Hello "+ name + " you are " + age + " years old and you are a " + occupation)
